e96c99abe9a641a0c8e6fad0c7103e63da53238a
Christian Catrofe <christianlopes100@outlook.com>
2024-01-02 21:16:54 -0300

    First Commit

---
+++ src/main/java/com/br/easytickets/EasyTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.br.easytickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyTicketsApplication.class, args);
+    }
+
+}
